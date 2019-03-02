68dd7fa98a89961430703813842af41ff00ac951
LeShawn Edouard <leshawnedouard92@gmail.com>
2019-03-02 16:13:34 -0500

    Converting to Bootstrap and added references in correct places

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,7 +12,15 @@ var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 // For loop to assign a number for the crystals //
 for (var i = 0; i < numberOptions.length; i++) {
 
-    var imageCrystal
+    var imageCrystal = $("<img>");
+
+    imageCrystal.addClass("crystal-image");
+
+    imageCrystal.attr("src", "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/space-infinity-stone-poster-style-1-cole-iba.jpg");
+
+    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+    $("#crystals").append(imageCrystal);
 
 // Shows the number of wins/losses the player has. //
 var wins = 0;
