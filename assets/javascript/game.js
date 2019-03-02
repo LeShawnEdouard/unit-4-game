b37f91966229bbfa09eee2d6a89f2f29bde58107
LeShawn Edouard <leshawnedouard92@gmail.com>
2019-03-02 07:55:35 -0500

    Removing text/content from html file and will 'append' the text via js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+// Will show the title of the game. //
+$("title").append()
+
+
+
+
+// Target number for the player to target. //
+var targetNumber = 
+
+$("random-number").text(targetNumber);
+
+
+// Shows the number of wins/losses the player has. //
+var wins = 0;
+var losses = 0;
+
+$("wins").text(wins);
+$("losses").text(losses);
