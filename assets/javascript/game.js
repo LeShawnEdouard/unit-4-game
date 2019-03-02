90aa7a4f03e1aa50cefb9690e9e3eeb3c61476c0
LeShawn Edouard <leshawnedouard92@gmail.com>
2019-03-02 14:03:42 -0500

    Added for loop, variables, and array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,18 @@
-// Will show the title of the game. //
-$("title").append()
-
-
-
 
 // Target number for the player to target. //
 var targetNumber = 
 
 $("random-number").text(targetNumber);
 
+var counter = 0;
+
+// Array that includes the values for crystals //
+var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+// For loop to assign a number for the crystals //
+for (var i = 0; i < numberOptions.length; i++) {
+
+    var imageCrystal
 
 // Shows the number of wins/losses the player has. //
 var wins = 0;
