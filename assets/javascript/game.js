c6f53b63018da0327cae70933bab4b985dfceaf7
LeShawn Edouard <leshawnedouard92@gmail.com>
2019-03-03 15:20:27 -0500

    Corrected references to run jquery library. Values are now added to images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,12 +7,7 @@
 // $("wins").text(wins);
 // $("losses").text(losses);
 
-
-
-$("#random-number").append(targetNumber);
-
 var counter = 0;
-
 
 
 
@@ -20,56 +15,37 @@ var counter = 0;
     // Math.floor used to randomize numbers between instructed ranges //
     // .attr used to assign values to the crystals //
     var targetNumber = Math.floor(Math.random() * 101 + 19);
-    blueValue = Math.floor(Math.random() * 12 + 1);
-    greenValue = Math.floor(Math.random() * 12 + 1);
-    orangeValue = Math.floor(Math.random() * 12 + 1);
-    redValue = Math.floor(Math.random() * 12 + 1);
-    totalScore = 0;
+    var blueValue = Math.floor(Math.random() * 12 + 1);
+    var greenValue = Math.floor(Math.random() * 12 + 1);
+    var orangeValue = Math.floor(Math.random() * 12 + 1);
+    var redValue = Math.floor(Math.random() * 12 + 1);
+    var totalScore = 0;
     console.log(targetNumber)
     console.log(blueValue)
     console.log(greenValue)
     console.log(orangeValue)
     console.log(redValue)
+
     $("#space").attr("value", blueValue);
     $("#time").attr("value", greenValue);
     $("#soul").attr("value", orangeValue);
     $("#reality").attr("value", redValue);
 
-
+    $("#random-number").text(targetNumber);
     $("#space").append(blueValue);
     $("#time").append(greenValue);
     $("#soul").append(orangeValue);
     $("#reality").append(redValue);
+    
+
 // }
 
 
 
-$("#crystal-button").on("click", function() {
+$("button#space").on("click", function() {
 
     // var crystalValue = ($(this).attr("value"));
     // crystalValue = parseInt(crystalValue);
-    
-    // for (var i = 0; i < numberOptions.length; i++) {
-    //     // Math.floor used to randomize numbers between instructed ranges //
-    //     // .attr used to assign values to the crystals //
-    //     targetNumber = Math.floor(Math.random() * 101 + 19);
-    //     blueValue = Math.floor(Math.random() * 12 + 1);
-    //     greenValue = Math.floor(Math.random() * 12 + 1);
-    //     orangeValue = Math.floor(Math.random() * 12 + 1);
-    //     redValue = Math.floor(Math.random() * 12 + 1);
-    //     totalScore = 0;
-    
-    //     $("#space").attr("value", blueValue);
-    //     $("#time").attr("value", greenValue);
-    //     $("#soul").attr("value", orangeValue);
-    //     $("#reality").attr("value", redValue);
-    
-    
-    //     $("#space").append(blueValue);
-    //     $("#time").append(greenValue);
-    //     $("#soul").append(orangeValue);
-    //     $("#reality").append(redValue);
-    // }
 
     counter += "value";
 
@@ -83,19 +59,3 @@ $("#crystal-button").on("click", function() {
       alert("You lose!!");
     }
 });
-
-
-
-
-// for (var i = 0; i < numberOptions.length; i++) {
-    
-//     var imageCrystal = $("<img>");
-
-//     imageCrystal.addClass("crystal-image");
-
-//     imageCrystal.attr("src", "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/space-infinity-stone-poster-style-1-cole-iba.jpg");
-
-//     imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-//     $("#space-crystal").append(imageCrystal);
-// }
