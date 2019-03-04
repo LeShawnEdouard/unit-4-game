7c979651aa4ac14fbcd826b0c31069d097f64da0
LeShawn Edouard <leshawnedouard92@gmail.com>
2019-03-04 01:12:20 -0500

    Fixed function to add total values and match the given number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,47 +7,43 @@
 // $("wins").text(wins);
 // $("losses").text(losses);
 
+
+
+// Math.floor used to randomize numbers between instructed ranges //
+// .attr used to assign values to the crystals //
+var targetNumber = Math.floor(Math.random() * 101 + 19);
+var blueValue = Math.floor(Math.random() * 12 + 1);
+var greenValue = Math.floor(Math.random() * 12 + 1);
+var orangeValue = Math.floor(Math.random() * 12 + 1);
+var redValue = Math.floor(Math.random() * 12 + 1);
 var counter = 0;
+console.log(targetNumber)
+console.log(blueValue)
+console.log(greenValue)
+console.log(orangeValue)
+console.log(redValue)
 
+$("#space").attr("value", blueValue);
+$("#time").attr("value", greenValue);
+$("#soul").attr("value", orangeValue);
+$("#reality").attr("value", redValue);
 
-
-// for (var i = 0; i < numberOptions.length; i++) {
-    // Math.floor used to randomize numbers between instructed ranges //
-    // .attr used to assign values to the crystals //
-    var targetNumber = Math.floor(Math.random() * 101 + 19);
-    var blueValue = Math.floor(Math.random() * 12 + 1);
-    var greenValue = Math.floor(Math.random() * 12 + 1);
-    var orangeValue = Math.floor(Math.random() * 12 + 1);
-    var redValue = Math.floor(Math.random() * 12 + 1);
-    var totalScore = 0;
-    console.log(targetNumber)
-    console.log(blueValue)
-    console.log(greenValue)
-    console.log(orangeValue)
-    console.log(redValue)
-
-    $("#space").attr("value", blueValue);
-    $("#time").attr("value", greenValue);
-    $("#soul").attr("value", orangeValue);
-    $("#reality").attr("value", redValue);
-
-    $("#random-number").text(targetNumber);
-    $("#space").append(blueValue);
-    $("#time").append(greenValue);
-    $("#soul").append(orangeValue);
-    $("#reality").append(redValue);
+$("#random-number").text(targetNumber);
+$("#space").append(blueValue);
+$("#time").append(greenValue);
+$("#soul").append(orangeValue);
+$("#reality").append(redValue);
+$("#total-score").append(counter);
     
 
-// }
 
 
+$(".crystal-button").on("click", function() {
 
-$("button#space").on("click", function() {
+    var crystalValue = ($(this).attr("value"));
+    crystalValue = parseInt(crystalValue);
 
-    // var crystalValue = ($(this).attr("value"));
-    // crystalValue = parseInt(crystalValue);
-
-    counter += "value";
+    counter += crystalValue;
 
     alert("New score: " + counter);
 
