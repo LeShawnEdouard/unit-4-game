796bdea879ed88f694fc4faa90ecefa37414b40b
LeShawn Edouard <leshawnedouard92@gmail.com>
2019-03-07 19:32:35 -0500

    Adding second audio as background music

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,8 +6,14 @@ var greenValue = Math.floor(Math.random() * 12 + 1);
 var orangeValue = Math.floor(Math.random() * 12 + 1);
 var redValue = Math.floor(Math.random() * 12 + 1);
 var counter = 0;
+
+// Audio files to play as background music //
 var audio = document.getElementById("thanos-voice");
 audio.play();
+var audioTwo = document.getElementById("background-music");
+setTimeout(function () {
+  alert("Hello");
+}, 15000);
 
 
 // Using an attribute to add values to the crystal buttons //
@@ -20,59 +26,56 @@ $("#reality").attr("value", redValue);
 // Will display the random number to match //
 $("#random-number").text(targetNumber);
 
-// var winAlert = "";
-// $(".alert alert-danger").hide(winAlert)
-
 
 // Variables that will help display number of wins/losses //
 var wins = 0;
 var losses = 0;
 
-    
+
 // Function to talley the values together //
 // Function to determine whether the total number
 // matchs the target number or not //
-$(".crystal-button").on("click", function() {
-    console.log(targetNumber);
-    var crystalValue = ($(this).attr("value"));
-    crystalValue = parseInt(crystalValue);
+$(".crystal-button").on("click", function () {
+  console.log(targetNumber);
+  var crystalValue = ($(this).attr("value"));
+  crystalValue = parseInt(crystalValue);
 
-    counter += crystalValue;
+  counter += crystalValue;
 
-    $("#total-score").text(counter);
+  $("#total-score").text(counter);
+  $("#wins").text(wins);
+  $("#losses").text(losses);
+
+
+  // Function to reset the game //
+  function setGame() {
+    counter = 0;
+    targetNumber = Math.floor(Math.random() * 101 + 19);
+    blueValue = Math.floor(Math.random() * 12 + 1);
+    greenValue = Math.floor(Math.random() * 12 + 1);
+    orangeValue = Math.floor(Math.random() * 12 + 1);
+    redValue = Math.floor(Math.random() * 12 + 1);
+    $("#random-number").text(targetNumber);
+    $("#space").attr("value", blueValue);
+    $("#time").attr("value", greenValue);
+    $("#soul").attr("value", orangeValue);
+    $("#reality").attr("value", redValue);
+    $("#total-score").text(0);
+  }
+
+  // If total-score matches random number condition //
+  if (counter === targetNumber) {
+    wins++;
     $("#wins").text(wins);
+    setGame();
+    console.log(setGame);
+  }
+
+  // If total-score is greater than random number condition //
+  else if (counter > targetNumber) {
+    losses++;
     $("#losses").text(losses);
-
-
-    // Function to reset the game //
-    function setGame() {
-      counter = 0;
-      targetNumber = Math.floor(Math.random() * 101 + 19);
-      blueValue = Math.floor(Math.random() * 12 + 1);
-      greenValue = Math.floor(Math.random() * 12 + 1);
-      orangeValue = Math.floor(Math.random() * 12 + 1);
-      redValue = Math.floor(Math.random() * 12 + 1);
-      $("#random-number").text(targetNumber);
-      $("#space").attr("value", blueValue);
-      $("#time").attr("value", greenValue);
-      $("#soul").attr("value", orangeValue);
-      $("#reality").attr("value", redValue);
-      $("#total-score").text(0);
-    }
-
-    // If total-score matches random number condition //
-    if (counter === targetNumber) {
-      wins ++;
-      $("#wins").text(wins);
-      setGame();
-      console.log(setGame);
-    }
-
-    // If total-score is greater than random number condition //
-    else if (counter > targetNumber) {
-      losses ++;
-      $("#losses").text(losses);
-      setGame();
-      console.log(setGame);
-    }
+    setGame();
+    console.log(setGame);
+  }
 });
